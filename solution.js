c1fd773e291a1023cad54eaa5e7dc8340838ab91
Rami B <ramibasheer9@gmail.com>
2022-06-30 23:07:00 +0200

    submitting answers

---
+++ solution.js
@@ -0,0 +1,43 @@
+const snackServings = 8;
+const guests = 4;
+console.log(snackServings + guests)
+console.log(snackServings * guests)
+console.log(snackServings / guests)
+
+const drinks = 10;
+console.log(snackServings * guests / drinks)
+const resultOne = 3.2;
+console.log(resultOne)
+
+const monkeys = 15;
+const bananas = 9;
+console.log(monkeys / bananas)
+
+const monkeyFights = 20;
+console.log(`${(monkeys + bananas) / monkeyFights}`)
+
+const resultTwo = 1.2;
+console.log(resultTwo);
+
+let monkeyBusiness = 90;
+monkeyBusiness++;
+console.log(monkeyBusiness);
+
+
+let boredom = 13;
+boredom--
+console.log(boredom);
+
+console.log(monkeys - bananas);
+const happiness = 6;
+const j3for = happiness + monkeyFights;
+console.log(j3for);
+
+console.log(resultOne % resultTwo)
+
+
+
+
+
+
+
